372b60b10494f7459d51da5f71cc60357d1a4b0a
NazarethV <nazarethvillar38@gmail.com>
2025-01-22 10:26:28 -0300

    App.js (Components)

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,27 @@
+export const getRentals = () => async (dispatch) => {
+    try {
+      const { data } = await axios.get('http://localhost:8080/api/v1/rentals');
+      dispatch({ type: 'GET_RENTALS', payload: data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  export const getRentalById = (id) => async (dispatch) => {
+    try {
+      const { data } = await axios.get(`http://localhost:8080/api/v1/rentals/${id}`);
+      dispatch({ type: 'GET_RENTAL_BY_ID', payload: data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  export const updateRental = (id, rentalData) => async (dispatch) => {
+    try {
+      const { data } = await axios.put(`http://localhost:8080/api/v1/rentals/${id}`, rentalData);
+      dispatch({ type: 'UPDATE_RENTAL', payload: data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
---
+++ src/views/home/Home.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRentals } from '../../redux/actions';
+
+const localizer = momentLocalizer(moment);
+
+function Home() {
+  const dispatch = useDispatch();
+  const rentals = useSelector((state) => state.rentals); // Alquileres desde el estado global.
+
+  useEffect(() => {
+    dispatch(getRentals()); // Cargar alquileres al inicio.
+  }, [dispatch]);
+
+  const events = rentals.map((rental) => ({
+    title: rental.tenantName,
+    start: new Date(rental.startDate),
+    end: new Date(rental.endDate),
+    id: rental.id,
+  }));
+
+  const handleSelectEvent = (event) => {
+    window.location.href = `/details/${event.id}`;
+  };
+
+  return (
+    <div>
+      <h1>Calendario de Alquileres</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        onSelectEvent={handleSelectEvent}
+      />
+    </div>
+  );
+}
+
+export default Home;
