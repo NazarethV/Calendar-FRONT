ff2f2fee061e14ddda741b9cf823492b2e4a51d4
NazarethV <nazarethvillar38@gmail.com>
2025-01-26 10:42:00 -0300

    config vite

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- // base: './', // Asegura que las rutas de los assets sean relativas
+  base: './', // Asegura que las rutas de los assets sean relativas
   build: {
     outDir: 'dist', // Define la carpeta de salida
   },
